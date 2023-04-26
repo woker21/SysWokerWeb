eab884d18c99e3d60f578e0c734ecd23eb12703d
woker21 <112359676+woker21@users.noreply.github.com>
2023-04-26 11:48:45 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
-import ContenedorServers from "./componentes/aplicacionServer";
 import "./css/container.css"
 
 
 function App() {
   return (
     <div className="App">
-      <ContenedorServers/>
+      
     </div>
   );
 }
