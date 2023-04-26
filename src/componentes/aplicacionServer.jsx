e51b6f7c4b0c7b77e863be48a3e189c30dbbfcd0
woker21 <112359676+woker21@users.noreply.github.com>
2023-04-26 11:30:57 +0000

    prueba

---
+++ src/componentes/aplicacionServer.jsx
@@ -0,0 +1,13 @@
+import ComponenteServer from "./servers";
+
+
+
+function ContenedorServers(){
+    return(
+        <div className="container1">
+            <ComponenteServer/>
+        </div>
+    );
+}
+
+export default ContenedorServers;
---
+++ src/componentes/servers.jsx
@@ -0,0 +1,97 @@
+
+
+
+
+function ComponenteServer() {
+    return (
+        <section>
+            <h1>Steam App</h1>
+            {/*CONTENEDOR PRINCIPAL DE LA APLICACION*/}
+            <div className="containerServer">
+                {/*INFORMACION DE SERVIDORES Y CANTIDAD DE USARIOS*/}
+                <div className="infoServer">
+                    <div className="informacion">
+                        {/*seccion de servidores*/}
+                        <div className="servers">
+                            <span>Online on Steam </span>
+                            <span>Status</span>
+                        </div>
+
+                        <div className="servers">
+                            <span>In-Game on Steam </span>
+                            <span>Status</span>
+                        </div>
+
+                        <div className="servers">
+                            <span>Steam Store</span>
+                            <span>Normal</span>
+                        </div>
+
+                        <div className="servers">
+                            <span>Steam Community </span>
+                            <span>Normal</span>
+                        </div>
+
+                        <div className="servers">
+                            <span>Steam Web API </span>
+                            <span>Normal</span>
+                        </div>
+
+                        <div className="servers">
+                            <span>Steam Connection Managers </span>
+                            <span>%</span>
+                        </div>
+
+                        <div className="servers">
+                            <span>TF2 Game Coordinator </span>
+                            <span>Normal</span>
+                        </div>
+
+                        <div className="servers">
+                            <span> Dota 2 Game Coordinator</span>
+                            <span>Normal</span>
+                        </div>
+
+                        <div className="servers">
+                            <span>CS Game Coordinator</span>
+                            <span>Normal</span>
+                        </div>
+
+                        <div className="servers">
+                            <span>CS Sessions Logon</span>
+                            <span>Normal</span>
+                        </div>
+
+                        <div className="servers">
+                            <span>CS Player Inventories</span>
+                            <span>Delayed</span>
+                        </div>
+
+                        <div className="servers">
+                            <span>CS Matchmaking Scheduler</span>
+                            <span>Normal</span>
+                        </div>
+
+                    <footer className="servers" id="footer">
+                        <p>Info de la aplicacion</p>
+                    </footer>
+                    </div>
+                {/*ESTADO DE LOS SERVIDORES*/}
+                <div className="servers">
+                    holas
+                </div>
+                </div>
+
+
+
+
+            </div>
+        </section>
+    );
+}
+
+
+
+
+
+export default ComponenteServer;
