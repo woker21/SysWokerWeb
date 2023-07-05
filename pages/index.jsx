8bbe397be7ec6a12a5923ca45b415b0fc3620ccc
Woker21 <112359676+woker21@users.noreply.github.com>
2023-07-05 22:25:18 +0200

    inicio

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,14 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import Menu from './componentes/menu';
 import Footer from './componentes/footer';
-import styles from '../styles/Home.module.css'
+import Lottie from "lottie-react";
+import AniPhone from '../images/phone.json';
+import styles from '../styles/Inicio.module.css'
 
 export default function Home() {
+  
+
+
   return (
     <>
     <Head>
@@ -13,10 +17,28 @@ export default function Home() {
     </Head>
     <Menu/>
     {/*Fin de Head y Menu*/}
-    <section className={styles.container}>
-      <h1>Inicio</h1>
-    </section>
+    <section className={styles.containerSeccion}>
 
+    <div className={styles.contenido}>
+    <div>
+    <h1>Diseñamos tus ideas y <br/> tus Negocios</h1>
+
+    <h3>un servicio totalmente <span>PERSONALIZADO</span>
+    <br /> 
+    donde planificaremos la marca de tu <br /> Emprendimiento
+    </h3>
+    <a>
+      <button>Contactanos</button>
+    </a>
+    </div>
+    </div>
+
+    <div className={styles.lotiPhone}>
+
+    <Lottie animationData={AniPhone} loop={true} />
+    </div>
+      
+    </section>
     <Footer/>
     </>
   )
